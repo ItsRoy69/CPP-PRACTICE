edc9b962eba06ec14532a7b313250de0e93482b4
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-10-28 20:00:44 +0530

    Updated

---
+++ C++ Intermediate/13.cpp
@@ -0,0 +1,16 @@
+//Increment & Decrement Operators
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=0;
+
+    //0 //0  //0   //1   //1  
+    i= i++ - --i + ++i - i--; 
+
+    cout<<i<<endl;
+    return 0;
+}
---
+++ C++ Intermediate/14.cpp
@@ -0,0 +1,20 @@
+//Increment & Decrement Operators
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1, j=2, k=3, m;
+
+       //1  //2    //3  
+    m= i-- - j-- - k--;
+
+    cout<<i<<endl;//0
+    cout<<j<<endl;//1
+    cout<<k<<endl;//2
+    cout<<m<<endl;//-4
+
+    return 0;
+}
