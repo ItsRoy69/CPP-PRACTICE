e4bdfe680127c9ea629fc1d71f645f711546492d
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-10-30 17:33:18 +0530

    Updated

--- C++ Intermediate/15.cpp
+++ C++ Intermediate/15.cpp
@@ -1,8 +1,25 @@
+//Rectangle Pattern
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int 
+    int row, col;
+
+    cout << "Enter the number of rows: ";
+    cin>>row;
+
+    cout << "Enter the number of columns: ";
+    cin>>col;
+
+    for(int i=0;i<=row;i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
