ebba90a145e302958d7eb90953e4864b6b8b2dcc
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-01-05 22:48:29 +0530

    Create 6.cpp

---
+++ C++ Complete/6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<<"Enter marks: "<<endl;
+    cin>>a;
+
+    if(a>90)
+        cout<<"Excellent"<<endl;
+    else if((a>80)&&(a<=90))
+        cout<<"Good"<<endl;
+    else if((a>70)&&(a<=80))
+        cout<<"Fair"<<endl;
+    else if((a>60)&&(a<=70))
+        cout<<"Meets Expectations"<<endl;
+    else
+        cout<<"Below Par"<<endl;
+}
