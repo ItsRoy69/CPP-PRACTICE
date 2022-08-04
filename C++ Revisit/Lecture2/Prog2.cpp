a2c8419e5b53a0d8bff6e971689d166b3ce25204
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-08-04 17:05:42 +0530

    prog2

---
+++ C++ Revisit/Lecture2/Prog2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a = 123;
+    cout << a << endl;
+
+    char b = 'l';
+    cout << b << endl;
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    float f = 1.23;
+    cout << f << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a is :" << size << endl;
+}
