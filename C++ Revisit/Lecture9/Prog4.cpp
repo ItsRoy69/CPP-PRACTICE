1c2e3673e8bd2c083269908cd6736bb9fdbe5ea9
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-08-16 11:25:39 +0530

    Lecture 9

---
+++ C++ Revisit/Lecture9/Prog4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int sumArr(int arr[],int n){
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int main() {
+
+    int a[100],n;
+
+    //Taking the size of the array
+    cout<<"Size of array is: ";
+    cin>>n;
+
+    //Taking the array from the user
+    cout<<"Elements in array: ";
+    for(int i = 0; i<n; i++) {
+        cin>>a[i];
+    }
+    cout<<"The sum of all the elements in the array is: " << sumArr(a,n) << endl;
+}
