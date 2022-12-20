94d7cca174d925fb1f68f45359271aa65dd8030d
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-12-20 22:23:24 +0530

    1,2

---
+++ Striver/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include <math.h>
+// #include <string>
+
+// using libraries which I'll be using
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!" << "\n";
+   cout << "Hello World!" << "\n";
+   return 0;
+}
