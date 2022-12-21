b6bcd6f56ae0c101176008f4b761e0c87fb5daff
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-12-21 22:00:48 +0530

    678

---
+++ Striver/6.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+    cout << s[2] << endl;
+
+}
+
+// Output:
+// "l" as it shows the third character of the string.
