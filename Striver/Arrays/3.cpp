c3f4c1309adcb1d6933fbc9106cec46a96c2373c
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-03-31 14:41:08 +0530

    arrays
    Signed-off-by: Jyotirmoy Roy <jyotirmoyroy649@gmail.com>

---
+++ Striver/Arrays/3.cpp
@@ -0,0 +1,31 @@
+// Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSorted(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5}, n = 5;
+    bool ans = isSorted(arr, n);
+    if (ans)
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    return 0;
+}
